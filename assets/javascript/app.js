3f5e82614298e49e3833786074977ee1f17d9183
Edyta <dudek002@hotmail.com>
2018-05-28 12:37:10 -0500

    resume added

---
+++ assets/javascript/app.js
@@ -0,0 +1,59 @@
+for (i=0; i<1000; i++) {
+  setTimeout(function(){
+    $(".hideLetter1").animate({ opacity: "1.0"  });  
+    }, 3000);
+  setTimeout(function(){
+    $(".hideLetter1").animate({ opacity: ".7"  });  
+    }, 3000);
+  setTimeout(function(){
+    $(".hideLetter1").animate({ opacity: ".4"  });  
+    }, 3000);
+  setTimeout(function(){
+    $(".hideLetter1").animate({ opacity: ".1"  });  
+    }, 3000);
+}
+
+for (i=0; i<1000; i++) {
+  setTimeout(function(){
+    $(".hideLetter2").animate({ opacity: ".7"  });  
+    }, 3000);
+  setTimeout(function(){
+    $(".hideLetter2").animate({ opacity: ".4"  });  
+    }, 3000);
+  setTimeout(function(){
+    $(".hideLetter2").animate({ opacity: ".1"  });  
+    }, 3000);
+  setTimeout(function(){
+    $(".hideLetter2").animate({ opacity: "1.0"  });  
+    }, 3000); 
+}
+
+for (i=0; i<1000; i++) {
+  setTimeout(function(){
+    $(".hideLetter3").animate({ opacity: ".4"  });  
+    }, 3000);
+  setTimeout(function(){
+    $(".hideLetter3").animate({ opacity: ".1"  });  
+    }, 3000);
+  setTimeout(function(){
+    $(".hideLetter3").animate({ opacity: "1.0"  });  
+    }, 3000);
+  setTimeout(function(){
+    $(".hideLetter3").animate({ opacity: "0.7"  });  
+    }, 3000); 
+}
+
+for (i=0; i<1000; i++) {
+  setTimeout(function(){
+    $(".hideLetter4").animate({ opacity: ".1"  });  
+    }, 3000);
+  setTimeout(function(){
+    $(".hideLetter4").animate({ opacity: "1"  });  
+    }, 3000);
+  setTimeout(function(){
+    $(".hideLetter4").animate({ opacity: "0.7"  });  
+    }, 3000);
+  setTimeout(function(){
+    $(".hideLetter4").animate({ opacity: "0.4"  });  
+    }, 3000);  
+}
